e5c5a4aac36c1e4f21a4966eb8a455392983eb2b
moro <ys7.xxxa@gmail.com>
2022-03-16 13:04:34 +0000

    [add 1622]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,54 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'shipping_address/index'
+    get 'shipping_address/edit'
+  end
+  namespace :public do
+    get 'orders/index'
+    get 'orders/show'
+    get 'orders/new'
+    get 'orders/log'
+    get 'orders/complete'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :admin do
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+  end
+  
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+  
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
