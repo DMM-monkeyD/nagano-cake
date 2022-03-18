f93cf02a3e000c7bf071106b261af014275be485
EC2 Default User <kcl988780@gmail.com>
2022-03-18 10:21:44 +0000

    [1819 add admin注文詳細ページ]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,6 +4,26 @@ class Admin::OrdersController < ApplicationController
   end
 
   def show
-    
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items.all
   end
+
+  def update
+    @order = Order.find(params[:id])
+    # 入金状態になったら全て製作待ちに
+    if @order.update(order_params)
+      if order_params[:status] == "confirm_deposit"
+        @order.order_items.update_all(making_status: 1)
+      end
+      redirect_to admin_order_path(@order)
+    else
+      render :show
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:status)
+  end
+
 end
