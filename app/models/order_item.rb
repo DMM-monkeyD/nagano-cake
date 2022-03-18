40462541b54bfa0deb12997e830f00d97318574a
moro <ys7.xxxa@gmail.com>
2022-03-18 05:22:53 +0000

    [add_1814]association

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,3 +1,6 @@
-class OrderItem < ApplicationRecord
+class OrderItem < 
+    belongs_to :order
+    belongs_to :item
+    
   enum making_status: { not_yet: 0, waiting_production: 1, in_production: 2, complete_production: 3 }
 end
