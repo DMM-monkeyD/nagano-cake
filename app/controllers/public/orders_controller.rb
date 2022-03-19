4f73e9bc772b699104a9e373829a205b1499ca2b
EC2 Default User <kcl988780@gmail.com>
2022-03-19 11:47:50 +0000

    [1920 add 会員側注文画面編集中]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,11 +1,14 @@
 class Public::OrdersController < ApplicationController
   def index
+    @orders = current_customer.orders.page(params[:page])
   end
 
   def show
   end
 
   def new
+    @order = Order.new
+    @customer = Customer.find(current_customer.id)
   end
 
   def log
