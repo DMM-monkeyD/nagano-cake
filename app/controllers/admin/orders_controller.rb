d7db0661c44d5167e84ec16714198ca07adb6d66
EC2 Default User <kcl988780@gmail.com>
2022-03-21 05:54:21 +0000

    [2114 fix 管理者側金額設定]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,6 +6,7 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @order_items = @order.order_items.all
+    @order_item = @order.order_items.find(params[:id])
   end
 
   def update
