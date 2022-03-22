60190aeab7e879dd8c02c270cffb8e5d677c9391
kjkubo <98828564+kjkubo@users.noreply.github.com>
2022-03-22 13:22:04 +0000

    2222　バリデーション設定

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -7,8 +7,12 @@ class Admin::GenresController < ApplicationController
 
   def create
     @genre = Genre.new(genre_params)
-    @genre.save
-    redirect_to edit_admin_genre_path(@genre.id)
+    if @genre.save
+    redirect_to admin_genres_path
+    else
+    @genres = Genre.all
+    render :index
+    end
   end
 
   def edit
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -17,8 +17,11 @@ class Admin::ItemsController < ApplicationController
   
   def create
     @item = Item.new(item_params)
-    @item.save
+    if @item.save
     redirect_to admin_items_path
+    else
+    render :new
+    end
   end
   
   def update
