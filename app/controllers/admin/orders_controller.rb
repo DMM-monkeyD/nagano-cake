cf4a6e6b03d9be77464d458ec308a838992c7012
EC2 Default User <kcl988780@gmail.com>
2022-03-23 12:06:30 +0000

    [2321 fix 注文詳細エラー修正]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,7 +7,6 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @order_items = @order.order_items.all
-    @order_item = @order.order_items.find(params[:id])
   end
 
   def update
